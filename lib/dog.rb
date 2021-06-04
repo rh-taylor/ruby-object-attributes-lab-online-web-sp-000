1962e630606184b13f100eaf752fc206b17322f9
Robyn Taylor <rhtaylor0715@gmail.com>
2021-06-04 18:38:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,16 @@
 class Dog
-  
-  def name=(dogs_name)
-    @name = dogs_name
+  def name=(name)
+    @name = name
   end
   
   def name
     @name
   end
   
-  def breed=(dogs_breed)
-    @breed = dogs_breed
+  def breed=(breed)
+    @breed = breed
   end
-  
+    
   def breed
     @breed
   end
